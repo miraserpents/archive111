33275f8c6decea2872263c3d4b9570a35420bbaa
Mirabela <mirabelarepesciuc111@gmail.com>
2025-05-31 14:49:01 -0700

    edited javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "mirabela";
+let age = "1111";
+let likesSushi = false;
+
+function greeting(name) {
+    console.log("hello", name);
+}
+
+greeting("mira");
+
+let newName = document.getElementbyId("greeter").value;
